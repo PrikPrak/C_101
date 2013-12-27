7fcff1a138dff72527d494dde4e7b10d54a21a6d
PrikPrak <PrikPrak@users.noreply.github.com>
2013-12-26 22:42:14 -0500

    Answered question in p11/main.c.

--- p11/main.c
+++ p11/main.c
@@ -4,7 +4,9 @@
 /* Problem 11: Equal to Zero?                                                  */
 /* Created On: 12/26/2013                                                      */
 /* Comments:                                                                   */
-/* QUESTION: Why is character input = 0?                                       */
+/* QUESTION: Why does character input make result = 0?                         */
+/* ANSWER: Reached end of stream before any successful matches AKA early       */
+/* matching failure, 0 returned.                                               */
 /*******************************************************************************/
 
 #include <stdio.h>
@@ -22,7 +24,7 @@ int main(int argc, char* argv[])
   standard practice, or should I just provide the brackets? Code looks better, 
   but I'm not sure if many would be aware of the fact that the printf statement
   belongs to the if condition. Would appreciate some feedback, thanks! */
-  
+
   if (num == 0) printf("\nThe input entered, %d, is equal to zero.\n\n", num);
 
   else printf("\nThe number is not equal to zero.\n\n");
