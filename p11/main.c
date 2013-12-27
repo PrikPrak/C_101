5a0937c66e6be8640c16a5b948d864f888e023a0
PrikPrak <PrikPrak@users.noreply.github.com>
2013-12-26 19:59:15 -0500

    Completed Problem 11: Equal to Zero?

---
+++ p11/main.c
@@ -0,0 +1,32 @@
+/*******************************************************************************/
+/* File Name: main.c                                                           */
+/* Programmer: Corey Prak                                                      */
+/* Problem 11: Equal to Zero?                                                  */
+/* Created On: 12/26/2013                                                      */
+/* Comments:                                                                   */
+/* QUESTION: Why is character input = 0?                                       */
+/*******************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  int num;
+  num = 0;
+
+  printf("\nPlease enter an integer: ");
+  scanf("%d", &num);
+
+  /* If statements, without brackets, consider only the next statement as part
+  of the condition, which is why I placed the printf on the line. Is this
+  standard practice, or should I just provide the brackets? Code looks better, 
+  but I'm not sure if many would be aware of the fact that the printf statement
+  belongs to the if condition. Would appreciate some feedback, thanks! */
+  
+  if (num == 0) printf("\nThe input entered, %d, is equal to zero.\n\n", num);
+
+  else printf("\nThe number is not equal to zero.\n\n");
+
+  return 0;
+}
+
