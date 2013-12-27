e78832b31e65b60749c4e86ef4017515b8b4f2db
PrikPrak <PrikPrak@users.noreply.github.com>
2013-12-26 20:13:21 -0500

    Completed Problem 14: Argc

---
+++ p14/main.c
@@ -0,0 +1,17 @@
+/*******************************************************************************/
+/* File Name: main.c                                                           */
+/* Programmer: Corey Prak                                                      */
+/* Problem 14: Argc                                                            */
+/* Created On: 12/26/2013                                                      */
+/* Comments:                                                                   */
+/*******************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  printf("\n%d\n\n", argc);
+
+  return 0;
+}
+
