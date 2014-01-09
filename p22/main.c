9cfac0dccb2889fadf23be291fb3264ce7d311ae
PrikPrak <PrikPrak@users.noreply.github.com>
2014-01-08 20:04:53 -0500

    Completed Problem 22: Reverse the Command Line, also edited comment.txt

--- p22/main.c
+++ p22/main.c
@@ -1,9 +1,27 @@
 /*******************************************************************************/
 /* File Name: main.c                                                           */
 /* Programmer: Corey Prak                                                      */
-/* Problem #:                                                       */
+/* Problem 22: Reverse the Command Line                                        */
 /* Created On: 01/08/2014                                                      */
 /* Comments:                                                                   */
 /*******************************************************************************/
 
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  int i;
+  i = 0;
+
+  printf("\nArgv Reversed:");
+
+  for (i = (argc - 1); i >= 0; i--)
+  {
+    printf("\n%s", argv[i]);
+  }
+
+  printf("\n\n");
+
+  return 0;
+}
 
